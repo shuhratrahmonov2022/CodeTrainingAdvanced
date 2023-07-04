e13992a1474fb585afe8450f4b6b097840b122ea
Shuhrat Rahmonov <shuhrat@silkroadprofessionals.com>
2023-07-04 15:32:02 +0500

    Calculator

---
+++ 01.SimpleConsoleCalculatorProject/IncrementDecrement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.SimpleConsoleCalculatorProject
+{
+    public class IncrementAndDecrement
+    {
+        public void MyMethod()
+        {
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            firstNumber %= 10;
+            Console.WriteLine(firstNumber);
+        }
+    }
+}
--- 01.SimpleConsoleCalculatorProject/Program.cs
+++ 01.SimpleConsoleCalculatorProject/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+
+namespace CalculatR
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            // + - / *
+            Console.WriteLine($"Adding {firstNumber + secondNumber}");
+            Console.WriteLine($"Substracting {firstNumber - secondNumber}");
+            Console.WriteLine($"Deviding {firstNumber / secondNumber}");
+            Console.WriteLine($"Multiplying {firstNumber * secondNumber}");
+
+            // %
+            Console.WriteLine($"Qoldiq {firstNumber % secondNumber}");
+        }
+    }
+}
