30c878973e2b1e310930765b3dfbe851974f6409
Shuhrat Rahmonov <shuhrat@silkroadprofessionals.com>
2023-07-04 17:27:52 +0500

    infra upload projects

---
+++ 02.SimplePrintConsoleProject/07.Feature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.SimplePrintConsoleProject
+{
+    public class FirstFeature
+    {
+        public void Feature(int age)
+        {
+            int Age = 3;
+            int ageDeference = age - Age;
+            Console.WriteLine($"Age Deference: {ageDeference}");
+        }
+    }
+}
