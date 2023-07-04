9444f94e6447beecd8a095aa6b6e0d7305d67fbf
Shuhratjon <shuhratrahmonov2022@gmail.com>
2023-07-04 08:39:40 +0500

    Initialize Integers Numbers

---
+++ 02.SimplePrintConsoleProject/02.Integers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.SimplePrintConsoleProject
+{
+    public class Integers
+    {
+        public void MyIntegersNumber()
+        {
+            string name = Console.ReadLine();
+            short age = 22;
+
+            //interpolated string
+            string greeting = $"Hello! {name}";
+
+            Console.WriteLine($"Your age is  probably {age}");
+     
+            // short,   int,   long
+            //syntax
+            short studentCount = 30;
+            int populationCount = 40_000;
+            long worldPopulation = 7_000_000_000;
+
+            // size
+            Console.WriteLine($"The size of short data type: {sizeof(short)}");
+            Console.WriteLine($"The size of int data type: {sizeof(int)}");
+            Console.WriteLine($"The size of long data type: {sizeof(long)}");
+
+            // range
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+
+            //unsigned
+            Console.WriteLine(ushort.MinValue);
+            Console.WriteLine(ushort.MaxValue);
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(uint.MaxValue);
+            Console.WriteLine(ulong.MinValue);
+            Console.WriteLine(ulong.MaxValue);
+
+        }
+    }
+}
